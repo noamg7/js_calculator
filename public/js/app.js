a20212005a6d99f0e496246809a7f4b4836eff84
NoamG <NoamG>
2016-02-10 21:04:41 -0500

    calculater

--- public/js/app.js
+++ public/js/app.js
@@ -14,35 +14,76 @@ $(document).ready(function(){
   $('mem-minus').on('click', memSub);
   $('mem-recall').on('click', memDisplay);
   $('mem-clear').on('click', memClear);
-  $('#plus-minus').on('click', plusMinus);
   $('#period').on('click', period);
   $('#percentage').on('click', percentage);
   $('#square').on('click', square);
   $('#squareRoot').on('click', squareRoot);
 
-  }
   function updateDisplay(){
-    var thing  = $(this).text();
-    console.log(thing);
+    var num = $(this).text();
+    if ($display.text() === '0') {
+      $display.text(num);
+  }else{
+    $display.text($display.text()+num);
+  }
+    console.log(num);
   }
   function divide(){
-
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = '/';
   }
 
   function multiply(){
-
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = '*';
   }
 
   function subtract(){
-
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = '-';
   }
 
   function equal(){
-
+    var displayText = $display.text();
+    var num = parseFloat(displayText);
+    if(lastOperation === '+'){
+      calculator.add(num);
+      var result = calculator.result();
+      $display.text(result);
+      calculator.reset();
+    } else if(lastOperation === '-'){
+      calculator.subtract(num);
+      var result = calculator.result();
+      $display.text(result);
+      calculator.reset();
+    } else if (lastOperation === '/'){
+      calculator.divide(num);
+      var result = calculator.result();
+      $display.text(result);
+      calculator.reset();
+    } else if (lastOperation === '*') {
+      calculator.multiply(num);
+      var result = calculator.result();
+      $display.text(result);
+      calculator.reset();
+    }
   }
 
   function plus(){
-
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = '+';
   }
 
   function clear(){
@@ -64,9 +105,7 @@ $(document).ready(function(){
   function memDisplay(){
 
   }
-  function plusMinus(){
 
-  }
   function period(){
 
   }
@@ -74,9 +113,18 @@ $(document).ready(function(){
 
   }
   function square(){
-
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = 'square';
   }
   function squareRoot(){
+    var displayText = $display.text();
+    var displayNum = parseFloat(displayText);
+    calculator.add(displayNum);
+    $display.text(0);
+    lastOperation = 'squareRoot';
 
   }
 });
